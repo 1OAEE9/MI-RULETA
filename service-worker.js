76608f8a1f1645a4f2dcc559863f0534d25cbb57
1OAEE9 <jdph95@gmail.com>
2025-10-19 17:09:23 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,22 @@
-const cacheName = 'ruleta-cache-v1';
-const filesToCache = [
-  '/',
+const CACHE_NAME = 'ruleta-cache-v1';
+const FILES = [
   '/index.html',
   '/manifest.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('activate', evt => {
+  evt.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
   );
 });
